9b1a98c2113603bb2bcd884eb92a81e5cffea6d0
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-03-03 03:07:29 +0000

    WIP calculating rotation

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const POINT_RADIUS = 8;
 const POINT_STROKE_WIDTH = 32;
 const POLYGON_STROKE_WIDTH = 4;
 const SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
+const SVG_REVERSED_Y_AXIS = -1;
 
 class App {
   constructor() {
@@ -16,12 +17,12 @@ class App {
     this.dataLayer = document.getElementById('data-layer');
     
     this.polygon = {
-      points: [200, 300, 400, 0, 600, 150, 800, 0, 1000, 300, 600, 600],
+      points: [250,300, 400,50, 600,200, 800,50, 950,300, 600, 550],
     };
     
     this.point = {
-      x: 400,
-      y: 200,
+      x: 600,
+      y: 300,
     };
     
     this.update();
@@ -39,14 +40,64 @@ class App {
     polygon.setAttribute('fill', 'none');
     this.dataLayer.appendChild(polygon);
     
+    for (let i = 0; (i+1) < this.polygon.points.length; i += 2) {
+      const text = document.createElementNS(SVG_NAMESPACE, 'text');
+      text.setAttribute('x', this.polygon.points[i]);
+      text.setAttribute('y', this.polygon.points[i + 1]);
+      text.setAttribute('fill', '#000');
+      text.textContent = Math.floor(i  / 2);
+      this.dataLayer.appendChild(text);
+    }
+    
+    const pointInPolygon = this.isPointInPolygon(this.point, this.polygon)
+    
     const point = document.createElementNS(SVG_NAMESPACE, 'circle');
     point.setAttribute('cx', this.point.x);
     point.setAttribute('cy', this.point.y);
     point.setAttribute('r', POINT_RADIUS);
     point.setAttribute('stroke-width', POINT_STROKE_WIDTH);
     point.setAttribute('stroke', 'rgba(192, 192, 192, 0.5)');
-    point.setAttribute('fill', 'rgba(0, 255, 0)');
+    
+    if (!pointInPolygon) {
+      point.setAttribute('fill', '#0e0');
+    } else {
+      point.setAttribute('fill', '#e00');
+    }
     this.dataLayer.appendChild(point);
+  }
+  
+  isPointInPolygon (point, polygon) {
+    if (!point || point.x === undefined || point.y === undefined
+        || !polygon || !polygon.points
+        || polygon.points.length < 6 || polygon.points.length % 2 !== 0) return false;
+    
+    let x2 = polygon.points[polygon.points.length - 2];
+    let y2 = polygon.points[polygon.points.length - 1];
+    let prevAngle = Math.atan2(SVG_REVERSED_Y_AXIS * (y2 - point.y), x2 - point.x);  // Initial angle
+    console.log('INITIAL ANGLE: ', prevAngle * 180 / Math.PI)
+    let rotation = 0;  // Total rotation
+    
+    for (let i = 0; (i + 1) < polygon.points.length; i += 2) {
+      x2 = polygon.points[i];
+      y2 = polygon.points[i + 1];
+      const currentAngle = Math.atan2(SVG_REVERSED_Y_AXIS * (y2 - point.y), x2 - point.x)
+
+      // rotation += ???
+      
+      console.log('TODO: CALCULATE ROTATION')
+      
+      const debugLine = document.createElementNS(SVG_NAMESPACE, 'line');
+      debugLine.setAttribute('x1', this.point.x);
+      debugLine.setAttribute('y1', this.point.y);
+      debugLine.setAttribute('x2', x2);
+      debugLine.setAttribute('y2', y2);
+      debugLine.setAttribute('stroke-width', 2);
+      debugLine.setAttribute('stroke', 'rgba(128, 128, 128, 0.5)');
+      this.dataLayer.appendChild(debugLine);
+    }
+    
+    return false;
+    
   }
 }
 
